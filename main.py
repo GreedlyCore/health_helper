4b7205cb0f2ab4e8b3312c22b041a60b93bc9703
GreedlyCore <59867043+GreedlyCore@users.noreply.github.com>
2021-02-08 23:41:57 +0300

    Day1 finished

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
+# import telebot
+# from Include.sql import article_db
+# from keyboards import *
+# from bs4 import BeautifulSoup
+# import requests as rq
+# from sd_parser import *
+# from fake_headers import Headers
+# from random import choice, randint
+# from const import *
+# from text import text
+# from os import getcwd
+# from datetime import datetime
+# from time import time
+# import mysql.connector as sql
+# from sql import *
+
+from telebot import  *
 import telebot
-from Include.sql import article_db
 from keyboards import *
-from bs4 import BeautifulSoup
-import requests as rq
-from sd_parser import *
-from fake_headers import Headers
-from random import choice, randint
-from const import *
 from text import text
 from os import getcwd
-from datetime import datetime
-from time import time
-import mysql.connector as sql
-from sql import *
 
-header = Headers(
-    browser="chrome",  # Generate only Chrome UA
-    os="win",  # Generate ony Windows platform
-    headers=True  # generate misc headers
-)
+# header = Headers(
+#     browser="chrome",  # Generate only Chrome UA
+#     os="win",  # Generate ony Windows platform
+#     headers=True  # generate misc headers
+# )
 
 
 
 bot = telebot.TeleBot(TOKEN)
+
+
+class user:
+    def __init__(self, name, surname, patronymic, age, symptoms, address):
+        self.name = 'не заполнено'
+        self.surname = 'не заполнено'
+        self.patronymic = 'не заполнено'
+        self.age = 'не заполнено'
+        self.symptoms = 'не заполнено'
+        self.address = "не заполнено"
 
 
 
@@ -33,7 +49,8 @@ def main(message):
 
     # register for new users
     if message.chat.id not in user_db.get_users_id():
-        user_db.create(message.chat.id)
+        registration()
+        # user_db.create(message.chat.id)
 
     bot.register_next_step_handler(sent, menu_selector)
 
@@ -47,11 +64,7 @@ def callback_query(call):
     theme = call.data.split()[1:-1]
     article_id = call.data.split()[-1]
 
-    if '&' in theme:
-        del (theme[1])
-        theme = '_'.join([i.lower() for i in theme])
-    else:
-        theme = '_'.join([i.lower() for i in theme])
+
 
     article_db.rate(article_id, mark)
     user_db.update_count_rated(call.from_user.id, mark)
@@ -92,4 +105,60 @@ def menu_selector(message):
         bot.register_next_step_handler(sent, menu_selector)
 
 
-bot.polling()
+bot.polling(none_stop=True, interval=0)
+
+
+
+
+
+
+
+def options(message):
+    print(surName, firstName, middleName)
+
+    bot.send_message(message.chat.id, text['chooseSymptomes'],reply_markup=keyboard.choose_symptome())
+    bot.send_message(message.from_user.id, text['info']
+                     +"\n"+text['surname']+surName+'\n'+text['firstname']+firstName+'\n'+
+                     text['middlename']+middleName+'\n'+text['age']+Age)
+
+
+@bot.message_handler(commands=['start'])
+def getTextMessages(message):
+    global typed
+    bot.send_message(message.from_user.id, text["greet"])
+    bot.register_next_step_handler(message, options)
+
+def Get_FIO(message):
+    global middleName, firstName, surName
+    if message.text not in commands:
+        Message = message.text.split()
+        try:
+            surName = Message[0]
+            try:
+                firstName = Message[1]
+                try:
+                    middleName = Message[2]
+
+                except:
+                    middleName = 'не заполнено'
+                    surName = 'не заполнено'
+                    firstName = 'не заполнено'
+                    bot.send_message(message.from_user.id, text['wrongMiddlenameInput'])
+                    bot.register_next_step_handler(message, Get_FIO)
+                options(message)
+            except:
+                middleName = 'не заполнено'
+                surName = 'не заполнено'
+                firstName = 'не заполнено'
+                bot.send_message(message.from_user.id, text['wrongFirstnameInput'])
+                bot.register_next_step_handler(message, Get_FIO)
+        except:
+            middleName = 'не заполнено'
+            surName = 'не заполнено'
+            firstName = 'не заполнено'
+            bot.send_message(message.from_user.id, text['wrongSurnameInput'])
+            bot.register_next_step_handler(message, Get_FIO)
+    else:
+        bot.send_message(message.from_user.id, text['wrongMessageInput'])
+        bot.register_next_step_handler(message, Get_FIO)
+
