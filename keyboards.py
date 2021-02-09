b991b6e829cbc2a8b9dd7cfdc0301ffeb134d170
GreedlyCore <59867043+GreedlyCore@users.noreply.github.com>
2021-02-09 21:26:46 +0300

    Day2_Summary

--- keyboards.py
+++ keyboards.py
@@ -1,5 +1,6 @@
 from telebot.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
-from text import symptomes
+from text import symptomes, diseases
+
 
 class keyboard:
     @staticmethod
@@ -7,32 +8,51 @@ class keyboard:
         Array = list()
         list_kb = list()
         for i in range(len(symptomes)):
-            Array.append(InlineKeyboardButton(symptomes[i], callback_data=symptomes[i]))
+            Array.append(InlineKeyboardButton(symptomes[i], callback_data="1"+symptomes[i]))
             if not ((i+1) % 3):
                 list_kb.append(Array)
                 Array = []
         if Array not in list_kb:
             list_kb.append(Array)
-        #list_kb = [[InlineKeyboardButton(symptomes[i], callback_data=symptomes[i]) for i in range(j*3, j*3+2)] for j in range(3)]
-
-        btnlist = []
-        #for i in range(0, len(symptomes), 3):
-        #    btn = InlineKeyboardButton(text=symptomes[i], callback_data=symptomes[i])
-        #    btnlist.append(btn)
-        #    if (i+1) % 3 == 0:
-        #        kb.row(btnlist)
-        #        print(kb, btnlist)
-        #        btnlist.clear()
-            #kb.add(InlineKeyboardButton(symptomes[i], callback_data=symptomes[i]))
-            #kb.add(InlineKeyboardButton(symptomes[i+1], callback_data=symptomes[i+1]))
-            #kb.add(InlineKeyboardButton(symptomes[i+2], callback_data=symptomes[i+2]))
 
         return InlineKeyboardMarkup(list_kb)
 
-    #@staticmethod
-    #def settings():
-    #    kb = ReplyKeyboardMarkup(True)
-    #    kb.row('Статистика')
-    #    kb.row('🧨Сбросить рекомендации', '🧠Сбросить статистику')
-    #    kb.row('Назад')
-    #    return kb
+
+    @staticmethod
+    def choose_diseases():
+        Array = list()
+        list_kb = list()
+        for i in range(len(diseases)):
+            Array.append(InlineKeyboardButton(diseases[i], callback_data="0" + diseases[i]))
+            if not ((i + 1) % 3):
+                list_kb.append(Array)
+                Array = []
+        if Array not in list_kb:
+            list_kb.append(Array)
+
+        return InlineKeyboardMarkup(list_kb)
+    @staticmethod
+    def main():
+       kb = ReplyKeyboardMarkup(True)
+       kb.row('🏥Обращение🏥', '🎤Голосовое обращение🏥')
+       kb.row('⚙Настройки')
+       return kb
+
+    @staticmethod
+    def select_gender():
+        kb = ReplyKeyboardMarkup(True)
+        kb.row('Мужской', 'Женский')
+        return kb
+
+    @staticmethod
+    def gender():
+        Array = [[InlineKeyboardButton("Мужской", callback_data=1),
+                 InlineKeyboardButton("Женский", callback_data=0),]]
+        return InlineKeyboardMarkup(Array)
+    @staticmethod
+    def correctInfo():
+        Array = [[InlineKeyboardButton("Yes", callback_data="True"),
+                 InlineKeyboardButton("No", callback_data="False"),]]
+
+
+        return InlineKeyboardMarkup(Array)
--- main.py
+++ main.py
@@ -1,164 +1,153 @@
-# import telebot
-# from Include.sql import article_db
-# from keyboards import *
-# from bs4 import BeautifulSoup
-# import requests as rq
-# from sd_parser import *
-# from fake_headers import Headers
-# from random import choice, randint
-# from const import *
-# from text import text
-# from os import getcwd
-# from datetime import datetime
-# from time import time
-# import mysql.connector as sql
-# from sql import *
 
+from random import choice, randint
+import mysql.connector as sql
 from telebot import  *
 import telebot
 from keyboards import *
 from text import text
 from os import getcwd
+from user import user
+from const import *
+import mysql.connector as sql
+from sql import *
+bot = telebot.TeleBot(TOKEN)
 
-# header = Headers(
-#     browser="chrome",  # Generate only Chrome UA
-#     os="win",  # Generate ony Windows platform
-#     headers=True  # generate misc headers
-# )
-
+user = user()
 
 
 bot = telebot.TeleBot(TOKEN)
 
+# многоразовый отклик на кнопки в клаве
+# 1 = симптом
+# 0 = заболевание и весь спектр трамв
 
-class user:
-    def __init__(self, name, surname, patronymic, age, symptoms, address):
-        self.name = 'не заполнено'
-        self.surname = 'не заполнено'
-        self.patronymic = 'не заполнено'
-        self.age = 'не заполнено'
-        self.symptoms = 'не заполнено'
-        self.address = "не заполнено"
-
-
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    bot.answer_callback_query(callback_query_id=call.id, text=choice(text['rated_callback']))
+    if call.data[0] == '1':
+        user.setSymptomes(user.getSymptomes()+[call.data[1:]])
+    elif call.data[0] == "0":
+        user.setDiseases(user.getDiseases()+[call.data[1:]])
+    print(user.getSymptomes())
+    print(user.getDiseases())
 
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    sent = bot.send_message(message.chat.id, text['greet'], reply_markup=keyboard.main())
 
     # register for new users
-    if message.chat.id not in user_db.get_users_id():
-        registration()
-        # user_db.create(message.chat.id)
 
-    bot.register_next_step_handler(sent, menu_selector)
-
-
-# многоразовый отклик на кнопки в клаве
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-    bot.answer_callback_query(callback_query_id=call.id, text=choice(text['rated_callback']))
-
-    mark = float(call.data.split()[0])
-    theme = call.data.split()[1:-1]
-    article_id = call.data.split()[-1]
-
-
-
-    article_db.rate(article_id, mark)
-    user_db.update_count_rated(call.from_user.id, mark)
-    user_db.rate(call.from_user.id, theme, mark)
-    user_db.add_view(call.from_user.id, article_id, mark)
-
-
-def settings(message):
-    if message.text == '🧨Сбросить рекомендации':
-        user_db.reboot_coefs(message.chat.id)
-        sent = bot.send_message(message.chat.id, text['reboot'], reply_markup=keyboard.main())
-        bot.register_next_step_handler(sent, menu_selector)
+    #message.json['from']['id']
+    if message.json['from']['id'] not in user_db.get_users_id():
+    #-----------------------------------------------------REGISTRATION---------------------------------------------#
+        sent = bot.send_message(message.chat.id, text['greet_new'])
+        bot.register_next_step_handler(sent, getInitials)
+        print('User created')
     else:
-        sent = bot.send_message(message.chat.id, text['wrong'], reply_markup=keyboard.settings())
-        bot.register_next_step_handler(sent, settings)
+        sent = bot.send_message(message.chat.id, text['greet_old_beginning']+user_db.getName(message.chat.id)+text['greet_old_ending'])
+        bot.register_next_step_handler(sent, menu_selector)
+
+
+
+def symptomes_input(message):
+    if message.text.lower() == 'готово':
+        sent = bot.send_message(message.chat.id, text['chooseDiseases'], reply_markup=keyboard.choose_diseases())
+        bot.register_next_step_handler(sent, diseases_input)
+    else:
+        user.setSymptomes(user.getSymptomes()+message.text.split(', '))
+        bot.register_next_step_handler(sent, symptomes_input)
+
+def diseases_input(message):
+    if message.text.lower() == 'готово':
+        sent = bot.send_message(message.chat.id, text['chooseGeo'])
+        bot.register_next_step_handler(sent, geo_input)
+
+    else:
+        user.setDiseases(user.getDiseases() + message.text.split(', '))
+        bot.register_next_step_handler(sent, diseases_input)
+
+
+@bot.message_handler(content_types=["location"])
+def geo_input(message):
+    if message.location is not None:
+        print(message.location)
+        print("latitude: %s; longitude: %s" % (message.location.latitude, message.location.longitude)) #широта долгота
+        user.setAddress([message.location.latitude, message.location.longitude])
+        id = message.json['from']['id']
+        user_db.create_request(id, user_db.getName(id), user_db.getSurName(id), user_db.getMiddleName(id), user_db.getAge(id), user.getSymptomes(), user.getDiseases(), [message.location.latitude, message.location.longitude])
+    else:
+        sent = bot.send_message(message.chat.id, text['NoneGeo'])
+        bot.register_next_step_handler(sent, geo_input)
+
+
+
 
 
 @bot.message_handler(func=lambda message: True)
 def menu_selector(message):
-    if message.text == '📤Получить статью':
-            sent = bot.send_message(message.chat.id, text['back'], reply_markup=keyboard.main())
-            bot.register_next_step_handler(sent, menu_selector)
+    if message.text == '🏥Обращение🏥':
+            sent = bot.send_message(message.chat.id, text['chooseSymptomes'], reply_markup=keyboard.choose_symptome())
+            bot.register_next_step_handler(sent, symptomes_input)
 
-    elif message.text == '⚖Калибровка':
-        id = message.chat.id
-        sent = bot.send_message(id, text['calibration'], reply_markup=keyboard.main())
+    elif message.text == '🎤Голосовое обращение🏥':
+        pass
 
-        bot.register_next_step_handler(sent, menu_selector)
-
-    elif message.text == '⏰Рассылка':
-        sent = bot.send_message(message.chat.id, text['auto'], reply_markup=keyboard.main())
-        bot.register_next_step_handler(sent, menu_selector)
     elif message.text == '⚙Настройки':
-        sent = bot.send_message(message.chat.id, text['settings'], reply_markup=keyboard.settings())
-        bot.register_next_step_handler(sent, settings)
+        pass
     else:
-        sent = bot.send_message(message.chat.id, text['wrong'], reply_markup=keyboard.main())
+        sent = bot.send_message(message.chat.id, text['wrongMessageInput'], reply_markup=keyboard.main())
         bot.register_next_step_handler(sent, menu_selector)
+
+
+def checkForRussian(text):
+    for i in text.replace(" ", "").upper():
+        if 0 <= ord(i)-1040 <= 31:
+            continue
+        else:
+            return False
+    return True
+
+def getGender(message):
+    if checkForRussian(message.text):
+        #не включены остальные гендеры
+        user.setGender(1 if message.text == "Мужской" else 0)
+
+        sent = bot.send_message(message.from_user.id,f"{text['info']} \n\n "
+                                              f"{text['surname']} {user.getSurname()}\n"
+                                              f"{text['firstname']}{user.getName()} \n"
+                                              f"{text['middlename']}{user.getMiddleName()}\n"
+                                              f"{text['age']} {user.getAge()}\n"
+                                              f"{text['gender']}{'мужской' if user.getGender() == 1 else 'женский'}"
+                                ,reply_markup=keyboard.main())
+        #без болезней, это костыль
+        user_db.create(message.json['from']['id'], user.getName(), user.getSurname(), user.getMiddleName(), user.getAge(),
+        user.getGender())
+
+        bot.register_next_step_handler(sent, menu_selector)
+    else:
+        bot.send_message(message.from_user.id, text['wrongMessageInput'])
+        bot.register_next_step_handler(message, getGender)
+
+def getAge(message):
+    if 130 >= int(message.text) > 0 and type(eval(message.text)) !=float:
+        user.setAge(int(message.text))
+        sent = bot.send_message(message.from_user.id, text['genderInput'], reply_markup=keyboard.select_gender())
+        bot.register_next_step_handler(sent, getGender)
+    else:
+        sent = bot.send_message(message.from_user.id, text['wrongMessageInput'])
+        bot.register_next_step_handler(sent, getAge)
+
+
+def getInitials(message):
+    if len(message.text.split()) == 3 and message.text.replace(" ", "").isalpha() and checkForRussian(message.text):
+        user.setSurname(message.text.split()[0])
+        user.setName(message.text.split()[1])
+        user.setMiddleName(message.text.split()[2])
+        bot.send_message(message.from_user.id, text['ageInput'])
+        bot.register_next_step_handler(message, getAge)
+    else:
+        bot.send_message(message.from_user.id, text['wrongMessageInput'])
+        bot.register_next_step_handler(message, getInitials)
 
 
 bot.polling(none_stop=True, interval=0)
-
-
-
-
-
-
-
-def options(message):
-    print(surName, firstName, middleName)
-
-    bot.send_message(message.chat.id, text['chooseSymptomes'],reply_markup=keyboard.choose_symptome())
-    bot.send_message(message.from_user.id, text['info']
-                     +"\n"+text['surname']+surName+'\n'+text['firstname']+firstName+'\n'+
-                     text['middlename']+middleName+'\n'+text['age']+Age)
-
-
-@bot.message_handler(commands=['start'])
-def getTextMessages(message):
-    global typed
-    bot.send_message(message.from_user.id, text["greet"])
-    bot.register_next_step_handler(message, options)
-
-def Get_FIO(message):
-    global middleName, firstName, surName
-    if message.text not in commands:
-        Message = message.text.split()
-        try:
-            surName = Message[0]
-            try:
-                firstName = Message[1]
-                try:
-                    middleName = Message[2]
-
-                except:
-                    middleName = 'не заполнено'
-                    surName = 'не заполнено'
-                    firstName = 'не заполнено'
-                    bot.send_message(message.from_user.id, text['wrongMiddlenameInput'])
-                    bot.register_next_step_handler(message, Get_FIO)
-                options(message)
-            except:
-                middleName = 'не заполнено'
-                surName = 'не заполнено'
-                firstName = 'не заполнено'
-                bot.send_message(message.from_user.id, text['wrongFirstnameInput'])
-                bot.register_next_step_handler(message, Get_FIO)
-        except:
-            middleName = 'не заполнено'
-            surName = 'не заполнено'
-            firstName = 'не заполнено'
-            bot.send_message(message.from_user.id, text['wrongSurnameInput'])
-            bot.register_next_step_handler(message, Get_FIO)
-    else:
-        bot.send_message(message.from_user.id, text['wrongMessageInput'])
-        bot.register_next_step_handler(message, Get_FIO)
-
